6e3984963ce391e0f7bba05d28429e38e3a0383b
HGF <15879831437@163.com>
2021-06-08 17:48:06 +0800

    更新基础示例

--- js/basis.js
+++ js/basis.js
@@ -26,7 +26,41 @@ window.onload = function () {
                         id: 3,
                         name: '乒乓球'
                     }
-                ]
+                ],
+                rawHtml: `<span style="color: red;">rawHtml</span>`,
+                customId: 'custom1',
+                ok: true,
+                url: 'www.baidu.com',
+                attributeName: 'href',
+                eventName: 'click',
+                firstName: '',
+                lastName: '',
+                question: '',
+                answer: 'answer text?',
+                isActive: true,
+                error: null,
+                activeClass: 'active2',
+                errorClass: 'text-danger2',
+                styleObject: {
+                    color: 'red',
+                    fontSize: 18
+                },
+                baseStyles: {
+                    color: '#0f0',
+                    'font-size': 20 + 'px'   
+                },
+                overStyles: {
+                    marginTop: '20px'
+                },
+                some: true,
+                parentMessage: 'Parent',
+                myObject: {
+                    title: 'How to do lists in vue',
+                    author: 'gaofeng',
+                    date: '2021-06-08'
+                },
+                style1: 'height: 200px;overflow-y: auto;',
+                style2: 'height: 1000px;'
             }
         },
         mounted () {
@@ -34,9 +68,54 @@ window.onload = function () {
                 this.count++;
             }, 1000);
         },
+        computed: {
+            itemMessage () {
+                return this.items.length > 0 ? 'YES' : 'NO';
+            },
+            fullName: {
+                get () {
+                    return this.firstName + this.lastName
+                },
+                set (newValue) {
+                    const names = newValue.split('');
+                    this.firstName = names[0];
+                    this.lastName = names[names.length - 1];
+                }
+            },
+            classObject () {
+                return {
+                    active: this.isActive && !this.error,
+                    'text-danger': this.error && this.error.type === 'fatal'
+                }
+            }
+        },
+        watch: {
+            question (newQuestion, oldQuestion) {
+                if (newQuestion.indexOf('?') > -1) {
+                    this.getAnswer();
+                }
+            }
+        },
         methods: {
             reverseMessage () {
                 this.message = this.message.split('').reverse().join('');
+            },
+            getAnswer () {
+                this.answer = 'Thinking...';
+                axios
+                    .get('https://yesno.wtf/api')
+                    .then(response => {
+                        this.answer = response.data.answer
+                    })
+                    .catch(error => {
+                        this.answer = 'Error!Could not reach the API.' + error
+                    });
+            },
+            onScroll () {
+                console.log(1);
+            },
+            eventUp (event) {
+                console.log(event);
             }
         }
     };
@@ -48,5 +127,25 @@ window.onload = function () {
         template: `<li>{{ todo.name }}</li>`
     });
 
-    app.mount('#helloVue');
+    app.component('my-component', {
+        template: `
+            <p class="foo bar">Hi!</p>
+            <span :class="$attrs.class">inline-block</span>
+        `
+    });
+
+    app.component('my-component2', {
+        template: `
+            <div>
+                {{ name }}
+                <button @click="$emit('remove')">Remove</button>
+            </div>
+        `,
+        props: ['name'],
+        emit: ['remove']
+    });
+
+    var vm = app.mount('#helloVue');
+
+    vm.fullName = 'jack';
 }
