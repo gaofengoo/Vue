58744f40811fce03c2d11534b22d191917789cd7
HGF <15879831437@163.com>
2021-06-16 17:52:15 +0800

    更新配置文件

--- js/deepComponent.js
+++ js/deepComponent.js
@@ -1,26 +1,26 @@
 window.onload = function () {
     const app = Vue.createApp({
-        components: {
-            'component-a': ComponentA,
-            'component-b': ComponentB,
-            'component-c': ComponentC
+        // components: {
+        //     'component-a': ComponentA,
+        //     'component-b': ComponentB,
+        //     'component-c': ComponentC
 
-        }
+        // }
     });
     // 局部组件
-    const ComponentA = {};
-    const ComponentB = {
-        // 在ComponentA中使用ComponentB
-        components: {
-            'component-a': ComponentA
-        }
-    };
-    const ComponentC = {};
+    // const ComponentA = {};
+    // const ComponentB = {
+    //     // 在ComponentA中使用ComponentB
+    //     components: {
+    //         'component-a': ComponentA
+    //     }
+    // };
+    // const ComponentC = {};
 
     // 全局组件
-    /*app.component('component-a', {});
+    app.component('component-a', {});
     app.component('component-b', {});
-    app.component('component-c', {});*/
+    app.component('component-c', {});
 
     app.mount('#myComponent');
 }
